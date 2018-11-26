995e0c43798586516b036414481f3a957962ad88
mspohrer <44450822+mspohrer@users.noreply.github.com>
2018-11-25 16:09:50 -0800

    with test examples

--- main.cpp
+++ main.cpp
@@ -3,18 +3,29 @@
 
 int main()
 {
-  cout << "Brute" << endl;
-  brute("the", "Algorithms and complexity.");
+     int len = 10000;
+    
+    string text(generateRandomBinary(len));
+    string pattern(text, 9980, 10);
+    struct timeval start, stop;
+    cout << endl << endl << endl;
 
-  cout << "KMP" << endl;
-  kmp("go", "Algorithms and complexity.");
+    gettimeofday(&start, NULL);
+    cout << "Brute" << endl;
+    brute(pattern, text);
+    gettimeofday(&stop, NULL);
+    cout << "Brtue took:" << stop.tv_usec - start.tv_usec << endl;
 
-  cout << "Rabin karp" << endl;
-  rabinKarp("go", "Algorithms and complexity.", 101);
-  generateStrings();
+    gettimeofday(&start, NULL);
+    cout << "KMP" << endl;
+    kmp(pattern, text);
+    gettimeofday(&stop, NULL);
+    cout << "KMP took:" << stop.tv_usec - start.tv_usec << endl;
+    
+    gettimeofday(&start, NULL);
+    cout << "Rabin karp" << endl;
+    rabinKarp(pattern, text, 101);
+    gettimeofday(&stop, NULL);
+    cout << "Rabin-Karp took:" << stop.tv_usec - start.tv_usec << endl;
 
-  std::cout << "BoyerMoore" << std::endl;
-  TestBoyerMoore testBoyerMoore;
-
-  return 0;
 }
