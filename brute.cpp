722f37cb072c38978a64ce9a1615659890b9b7c4
mspohrer <44450822+mspohrer@users.noreply.github.com>
2018-11-26 17:01:03 -0800

    Update brute.cpp

--- brute.cpp
+++ brute.cpp
@@ -5,21 +5,18 @@ void brute(string * pattern, string * text) {
     int pat_i = 0;
     int text_len = text->length();
     int pat_len = pattern->length();
-  
-    while (text_i < text_len - pat_len +1) {
+    int stop_i = text_len - pat_len;
+
+    for(text_i;text_i < stop_i; ++text_i){
         while (text->at(text_i + pat_i) == pattern->at(pat_i)) {
-            /*if (pat_i == pat_len - 1) {
-                return;
-                //cout << text_i  << ",";
-            }*/
             ++pat_i;
-            ++text_i;
+            if (pat_i == pat_len - 1) {
+                break;
+            }
+
         }
-        //resets the text index to where the comparisons started + 1
-        text_i -= pat_i;
-        ++text_i;
         pat_i = 0;
     }
-    //cout << endl;
+    cout << count;
     return;
 }
