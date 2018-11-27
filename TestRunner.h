f6b52af9c5ba0894fe2e169d17a6fec897ae17ee
bgfraleypdx <44735128+bgfraleypdx@users.noreply.github.com>
2018-11-26 21:54:57 -0800

    Scaled up the size of the test files.

--- TestRunner.h
+++ TestRunner.h
@@ -10,25 +10,21 @@
 class TestRunner
 {
   private:
-    void testBinary10k(ofstream &resultsFile);
-    void testBinary100k(ofstream &resultsFile);
     void testBinary1mil(ofstream &resultsFile);
     void testBinary10mil(ofstream &resultsFile);
+    void testBinary100mil(ofstream &resultsFile);
 
-    void testAlphanum10k(ofstream &resultsFile);
-    void testAlphanum100k(ofstream &resultsFile);
     void testAlphanum1mil(ofstream &resultsFile);
     void testAlphanum10mil(ofstream &resultsFile);
+    void testAlphanum100mil(ofstream &resultsFile);
 
-    void testSingle10k(ofstream &resultsFile);
-    void testSingle100k(ofstream &resultsFile);
     void testSingle1mil(ofstream &resultsFile);
     void testSingle10mil(ofstream &resultsFile);
+    void testSingle100mil(ofstream &resultsFile);
 
-    void testDNA10k(ofstream &resultsFile);
-    void testDNA100k(ofstream &resultsFile);
     void testDNA1mil(ofstream &resultsFile);
     void testDNA10mil(ofstream &resultsFile);
+    void testDNA100mil(ofstream &resultsFile);
 
     void testLargeBook(ofstream &resultsFile);
     void testBinaryAlternating(ofstream &resultsFile);
